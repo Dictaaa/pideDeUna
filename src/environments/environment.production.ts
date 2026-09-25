1bd41e8c0f79dda11def8f498d2f44fd885954a0
Dictaaa <daencovi@gmail.com>
2026-09-25 17:09:35 -0500

    2026092502

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:     'http://localhost:4000/api/v1',
+  apiUrl:     'https://pidedeunaback.onrender.com/api/v1',
   socketUrl:  'http://localhost:4000/',
   wompiPublicKey: 'pub_test_xxxxx',
 };
